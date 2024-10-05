b8814754577c4b97e0e30539e89b353e72a1659b
Ayush Priyam <ayushpriyam24@gmail.com>
2024-10-05 22:33:02 +0530

    db connected

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,17 @@
 import dotenv from "dotenv"
 import express from "express";
+import connectDB from "./config/dbConfig.js";
 
 dotenv.config();
 
 const app = express();
 
+connectDB();
+
+app.use(express.json());
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`app is running on port ${PORT}`);
+    console.log(`✅app is running on port ${PORT}`);
 })  
